79c42c61216040dd02ecd8ef696baf31c6ef01e3
Santiagobocel <santiagobocel10@gmail.com>
2020-01-27 15:42:39 -0600

    Constructores

---
+++ Lab2_arbolB/ArbolB/ArbolB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab2_arbolB.ArbolB
+{
+    public class ArbolB
+    {
+        private static ArbolB _instance = null;
+        public static ArbolB Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new ArbolB();
+                return _instance;
+            }
+        }
+
+        public void Add()
+        {
+
+        }
+        public void find()
+        {
+
+        }
+    }
+}
