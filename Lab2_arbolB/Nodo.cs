fb25bc0f8ce8ba7d4dce4a6a39932bc7553ca8e2
PabloMuralles <pablomuralles28@gmail.com>
2020-02-04 21:31:21 -0600

    arreglo de busqueda

--- Lab2_arbolB/Nodo.cs
+++ Lab2_arbolB/Nodo.cs
@@ -13,8 +13,9 @@ namespace Lab2_arbolB
         public Nodo[] hijos = new Nodo[grado];
 
 
-        public void Busqueda(string nombre)
+        public Bebidas Busqueda(string nombre)
         {
+            Bebidas BebidaBuscada = new Bebidas();
             for (int i = 0; i < grado-1; i++)
             {
                 int x = (String.Compare(nombre, datos[i].Name));
@@ -26,20 +27,46 @@ namespace Lab2_arbolB
 
                 if (String.Compare(nombre,datos[i].Name) == 1)
                 {
-                    if (String.Compare(nombre,datos[i+1].Name) == -1)
+                    if (datos[i+1]==null)
                     {
-                        hijos[i].Busqueda(nombre);
+                        hijos[i+1].Busqueda(nombre);
+
                     }
+                    else
+                    {
+                        if (String.Compare(nombre, datos[i + 1].Name) == -1)
+                        {
+                            hijos[i].Busqueda(nombre);
+                        }
+                    }
+                   
+                    
+                         
+
 
                 }
+               
 
                 if (String.Compare(nombre,datos[i].Name) == 0)
                 {
-                    // encontrado
+                     
+
+                    BebidaBuscada.Name = datos[i].Name;
+                    BebidaBuscada.flavor = datos[i].flavor;
+                    BebidaBuscada.inventory = datos[i].inventory;
+                    BebidaBuscada.price = datos[i].price;
+                    BebidaBuscada.Made= datos[i].Made;
+
+
+                    break;
+
                 }
 
+            
 
             }
+
+            return BebidaBuscada;
         }
     }
 }
