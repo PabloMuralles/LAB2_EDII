f7fff28cad018b74865b793d6512fcee5fad67c0
Santiagobocel <santiagobocel10@gmail.com>
2020-02-06 19:06:02 -0600

    arbol final 1.0.0

--- Lab2_arbolB/Almacenamiento/ArbolB.cs
+++ Lab2_arbolB/Almacenamiento/ArbolB.cs
@@ -111,11 +111,24 @@ namespace Lab2_arbolB.Almacenamiento
             if (inicial.hijos[num_hijos] != null)///
             {
                 int asignador = 0;
-                inicial.hijos[num_hijos+ 2] = new Nodo();
-                foreach (var item in inicial.hijos[num_hijos + 1].datos)
+                if (num_hijos == 0)
                 {
-                    inicial.hijos[num_hijos + 2].datos[asignador] = item;
-                    asignador++;
+                    inicial.hijos[num_hijos+ 2] = new Nodo();
+                    foreach (var item in inicial.hijos[num_hijos + 1].datos)
+                    {
+                        inicial.hijos[num_hijos + 2].datos[asignador] = item;
+                        asignador++;
+                    }
+                }
+                //////////////////
+                else 
+                {
+                    inicial.hijos[num_hijos + 1] = new Nodo();
+                    foreach (var item in inicial.hijos[num_hijos].datos)
+                    {
+                        inicial.hijos[num_hijos + 1].datos[asignador] = item;
+                        asignador++;
+                    }
                 }
                 Array.Clear(inicial.hijos[num_hijos+ 1].datos, 0, grado - 1);
             }
@@ -131,6 +144,7 @@ namespace Lab2_arbolB.Almacenamiento
                 }
                 if (cont+1>=inicial.datos.Length)
                 {
+                    ///
                     inicial.datos[cont] = nodo2[mitad];
                     Ordenar(ref inicial.datos);
                     break;
@@ -143,16 +157,17 @@ namespace Lab2_arbolB.Almacenamiento
                 }
                 cont++;
             }            
-            izq.padre = inicial;
             if (inicial.hijos[num_hijos] != null)
             {
             Array.Clear(inicial.hijos[num_hijos].datos,0,grado-1);
             }            
+            izq.padre = inicial;
             inicial.hijos[num_hijos] = izq; //index
             for (int llenado_izq = 0; llenado_izq < mitad; llenado_izq++)
             {
                 inicial.hijos[num_hijos].datos[llenado_izq] = nodo2[llenado_izq];                    
-            }            
+            }
+            der.padre = inicial;
             inicial.hijos[num_hijos + 1] = der; //index + 1 
             int contador = 0;
             for (int llenado_der = mitad + 1; llenado_der > mitad && llenado_der < nodo2.Length; llenado_der++)
@@ -161,58 +176,141 @@ namespace Lab2_arbolB.Almacenamiento
                 contador++;
             }
            }
-        public void insert_sheets(string N, string f, int i, double p, string M, Bebidas[] nodo,Nodo nuevo)
+            public void insert_sheets(string N, string f, int i, double p, string M, Bebidas[] nodo,Nodo nuevo)
         {
             int cont = 0;
-            foreach (var item in nodo) ///revisar el foreach con mas numeros
+            foreach (var item in nodo)
             {
+                if (cont == grado - 2)
+                {
+                    for (int contar_padres = 0; contar_padres < nuevo.datos.Length; contar_padres++)
+                    {
+                        if (N.CompareTo(nuevo.datos[contar_padres].Name) > 0)
+                        {
+                            asignar(N, f, i, p, M, cont + 1, 0, nuevo);
+                            break;
+                        }
+                    }
+                }
+                else 
+                { 
                 if (nodo[cont+1] == null)
                 {
                 #region hijos a la derecha
                 if (N.CompareTo(item.Name) > 0)
-                {
-                    int num = 0;
-                    asignar(N, f, i, p, M, cont+1 , num , nuevo);
+                {                   
+                    asignar(N, f, i, p, M, cont+1 , 0 , nuevo);
                     break;
                 }
                 #endregion
                 #region hijos a la izquierda
                 else
-                {
-                    int num = 0;
-                    asignar(N, f, i, p, M, cont, num, nuevo);
+                {                    
+                    asignar(N, f, i, p, M, cont, 0, nuevo);
                     break;
                 }
                 #endregion
                 }
                 cont++;
+                }
             }
         }
             public void asignar(string N, string f, int i, double p, string M,int cont ,int num, Nodo nuevo)
-            {
-            foreach (var nuevo_espacio in nuevo.hijos[cont].datos)
-            {
-                if (nuevo_espacio == null)
+            {           
+                foreach (var nuevo_espacio in nuevo.hijos[cont].datos)
                 {
-                    nuevo.hijos[cont].datos[num] = new Bebidas()
+                    if (nuevo_espacio == null)
                     {
-                        Name = N,
-                        flavor = f,
-                        inventory = i,
-                        price = p,
-                        Made = M,
-                    };
-                    break;
+                        nuevo.hijos[cont].datos[num] = new Bebidas()
+                        {
+                            Name = N,
+                            flavor = f,
+                            inventory = i,
+                            price = p,
+                            Made = M,
+                        };
+                        break;
+                    }
+                    num++;
+                    if (num == grado - 1) /// full
+                    {
+                         ///// componer tercer nivel
+                        if (cont == grado-1)
+                        {
+                            Aux(N, f, i, p, M, nuevo.hijos[cont].datos);
+                            Nueva_Raiz(auxiliar, nuevo);
+                        }
+                        else
+                        {
+                        Aux(N, f, i, p, M, nuevo.hijos[cont].datos);
+                        separar(ref nuevo.hijos[cont].datos, auxiliar, nuevo.hijos[cont].padre, cont);                       
+                        }
+                    }
                 }
-                num++;
-                if (num == grado - 1) /// full
-                {
-                    Aux(N, f, i, p, M, nuevo.hijos[cont].datos);
-                    separar(ref nuevo.hijos[cont].datos, auxiliar, nuevo.hijos[cont].padre,cont);
-                }
-              }
-              Ordenar(ref nuevo.hijos[cont].datos);
+                Ordenar(ref nuevo.hijos[cont].datos);                  
             }
+            public void Nueva_Raiz(Bebidas[] anterior, Nodo root)
+            {
+               var aux = new Nodo();
+               var momento = new Bebidas[grado];
+               var hijo_izq = new Bebidas[grado -1];
+               var hijo_der = new Bebidas[grado -1];
+                int mitad = (anterior.Length / 2);
+             int mitad_hijos = grado / 2;
+                aux.datos[0] = anterior[mitad];
+               aux.hijos[1] = new Nodo();
+                for (int x = 0; x < mitad; x++)
+                 {
+                  aux.hijos[1].datos[x] = anterior[x];
+                 }
+            aux.hijos[2] = new Nodo();
+            int contador = 0;
+               for (int y = mitad + 1; y > mitad && y < anterior.Length; y++)
+               {
+                aux.hijos[2].datos[contador] = anterior[y];
+                contador++;
+               }
+            int num = 0;
+            aux.hijos[0] = new Nodo();
+                 foreach (var item in root.hijos[mitad_hijos + 1].datos)
+                 {
+                   aux.hijos[0].datos[num] = item;
+                   num++;
+                 }
+            int entrar = 0;
+               foreach (var item in root.datos)
+               {
+                momento[entrar] = item;
+                    entrar++;
+               }
+              momento[entrar] = aux.datos[0];
+              Ordenar(ref momento);
+              mitad = momento.Length / 2;
+             hijo_izq = root.hijos[0].datos;
+             hijo_der = root.hijos[1].datos;
+              Array.Clear(root.datos, 0, grado - 1);
+              root.datos[0] = momento[mitad];
+               for (int Nx = 0; Nx < mitad; Nx++)
+               {
+                root.hijos[0].datos[Nx] = momento[Nx];
+               }
+              int start = 0;
+            for (int Ny = mitad + 1; Ny > mitad && Ny < momento.Length; Ny++)
+            {
+                root.hijos[1].datos[start] = momento[Ny];
+                start++;
+            }
+            ////////////////////
+              root.hijos[0].padre = root;
+              root.hijos[1].padre = root;
+              raiz.hijos[0].padre = root.hijos[0];
+              raiz.hijos[1].padre = root.hijos[0];
+              raiz.hijos[2].padre = root.hijos[0];
+              aux.hijos[0].padre = root.hijos[1];
+              aux.hijos[1].padre = root.hijos[2];
+              aux.hijos[2].padre = root.hijos[2];
+
+        }
         #endregion
 
         public Bebidas Buscar(string _nombre)
